c56abb6dca66f74866d0675c7e3f12a202cc6402
zalgo98 <92859073+zalgo98@users.noreply.github.com>
2026-03-25 16:45:15 +0100

    Add files via upload

---
+++ src/test/java/com/Proyect/SistemaPago/SistemaPagoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Proyect.SistemaPago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaPagoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
