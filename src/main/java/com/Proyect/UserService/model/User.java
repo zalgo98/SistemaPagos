b611020dddd98d3b82f22141389ce99f7c17db21
gonzalo <gonperezg@gmail.com>
2026-04-05 13:24:51 +0200

    clase user

--- src/main/java/com/Proyect/UserService/model/User.java
+++ src/main/java/com/Proyect/UserService/model/User.java
@@ -1,5 +1,50 @@
 package com.Proyect.UserService.model;
 
 public class User {
+    
+    private int Id;
+    private String Nombre;
+    private String Username;
+    private String Contraseña;
+
+    public User(int Id, String Nombre, String Username, String Contraseña){
+        this.Id=Id;
+        this.Nombre=Nombre;
+        this.Username=Username;
+        this.Contraseña=Contraseña;
+
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int Id) {
+        this.Id = Id;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String Username) {
+        this.Username = Username;
+    }
+
+    public String getContraseña() {
+        return Contraseña;
+    }
+
+    public void setContraseña(String Contraseña) {
+        this.Contraseña = Contraseña;
+    }
 
 }
