528af36c12549fc666f868c05007c4b107951026
gonzalo <gonperezg@gmail.com>
2026-04-06 20:22:48 +0200

    metodo post

--- src/main/java/com/Proyect/UserService/controller/UserController.java
+++ src/main/java/com/Proyect/UserService/controller/UserController.java
@@ -3,24 +3,34 @@ package com.Proyect.UserService.controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.Proyect.UserService.model.User;
 import com.Proyect.UserService.repository.UserRepository;
 
-import lombok.RequiredArgsConstructor;
-
 @RestController
 @RequestMapping("/users")
-@RequiredArgsConstructor
 
 public class UserController {
 
     private final UserRepository userRepository;
 
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
     @GetMapping
     public List<User> getUsers() {
         return userRepository.findAll();
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        userRepository.save(user);
+        return userRepository.findById(user.getId()).orElse(null);
     }
 }
